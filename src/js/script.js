e9b6af4b6bbe90f61e67255c6fffbd6aa27992e1
abdulaziz <abdulaziz.yosk@gmail.com>
2020-09-04 02:10:50 +0300

    created section resume

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,8 @@
 
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeMenu = document.querySelector('.menu__close');
+      closeMenu = document.querySelector('.menu__close'),
+      overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('active');
@@ -10,4 +11,10 @@ hamburger.addEventListener('click', () => {
 
 closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', e => {
+    if (e.target === overlay) {
+        menu.classList.remove('active');
+    }
 });
