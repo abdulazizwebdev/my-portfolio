4496d80891167bf09a79d7bd05655458232a8c90
abdulaziz <abdulaziz.yosk@gmail.com>
2020-09-10 10:43:10 +0300

    some bugs fixed

--- dist/js/script.js
+++ dist/js/script.js
@@ -27,3 +27,36 @@ const percentage = document.querySelectorAll('.skills__ratings-counter'),
 percentage.forEach((elem, i) => {
     line[i].style.width = elem.innerHTML;
 });
+
+// modal 
+
+// const modal = document.querySelector('.modal'),
+//       btnClose = document.querySelector('.modal__close'),
+//       btnSub = document.querySelector('.contacts__btn'),
+//       modalOverlay = document.querySelector('.overlay'),
+//       inputs = document.querySelectorAll('.contacts__input');
+
+// modalOverlay.classList.add('hide');
+// inputs.forEach(item => {
+//     if (!item.value === '') {
+//         openModal(btnSub);
+//     }
+// });
+
+// btnClose.addEventListener('click', () => {
+//     modal.classList.remove('show');
+//     modal.classList.add('hide');
+//     modalOverlay.classList.add('hide');
+//     modalOverlay.classList.remove('show');
+// });
+
+// function openModal(item) {
+//     item.addEventListener('click', () => {
+//         modal.classList.remove('hide');
+//         modal.classList.add('show');
+        
+//         modalOverlay.classList.remove('hide');
+//         modalOverlay.classList.add('show');
+//     });
+// }
+
